785a2f1ff00f3911eb21484d371cbf5e17cdfffb
nalvare84523 <nalvare84523@universidadean.edu.co>
2022-10-16 12:39:30 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import List from "./components/List";
+import { Sub } from "./types";
+
+interface AppState {
+  subs: Array<Sub>;
+  newSubsNumber: number;
+}
+
+const INITIAL_STATE = [
+  {
+    nick: "dapelu",
+    subMonths: 3,
+    avatar: "https://i.pravatar.cc/150?u=dapelu",
+    description: "Dapelu hace de moderador a veces",
+  },
+  {
+    nick: "sergio_serrano",
+    subMonths: 7,
+    avatar: "https://i.pravatar.cc/150?u=sergio_serrano",
+  },
+];
+
+function App() {
+  const [subs, setSubs] = useState<AppState["subs"]>([]);
+  const [newSubsNumber, setNewSubsNumber] =
+    useState<AppState["newSubsNumber"]>(0);
+
+  useEffect(() => {
+    setSubs(INITIAL_STATE);
+  }, []);
+
+  const handleNewSub = (newSub: Sub): void => {
+    setSubs(subs => [...subs, newSub]);
+  }
+
+  return (
+    <div className="App">
+      <h1>Nelson subs</h1>
+      <List subs={subs}/>
+      <Form onNewSub={handleNewSub} />
+    </div>
+  );
+}
+
+export default App;
